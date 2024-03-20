52450b9bcce11beae69b7d13894de6ea0a51de1a
rehema_funani <rehemafunani@gmail.com>
2024-03-20 21:55:46 +0300

    first commit

---
+++ array.js
@@ -0,0 +1,33 @@
+let arr1 = [3,7,34,90,12];
+console.log(arr1.slice(4));
+
+let arr2 = [true,"green","where",12,56]
+console.log(arr2.slice(4));
+
+let stringMyPets = ["cow","bad","snake","dog"];
+console.log(stringMyPets.join());
+
+let arr3 = [-5,9,5,3,2,-3,6,8,4,1];
+console.log(arr3.sort());
+
+var arr = ["apple","mango","apple","orange","mango","mango"];
+console.log("Duplicate numbers:");
+
+let arr5 = ["the","way","x",4];
+if(arr5.includes("way")){
+    console.log("way")
+}
+else{
+    console.log("the search word was not found")
+}
+
+let word = "sevink";
+let b =word.split("");
+console.log(b);
+console.log(b.sort());
+
+
+
+
+
+
